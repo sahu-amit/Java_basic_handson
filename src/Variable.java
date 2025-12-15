5ad41c61eb8a5ca728ca19d450464f406f3aad9c
Amit Sahu <sahuamitlu@gmail.com>
2025-12-16 00:26:33 +0530

    Pushing the handson Modules

---
+++ src/Variable.java
@@ -0,0 +1,31 @@
+public class Variable {
+    static void main() {
+        byte a = 127;
+        System.out.println("This is byte: "+a);
+        System.out.println("------------------");
+        short b = 130;
+        System.out.println("This is short: "+b);
+        System.out.println("------------------");
+        int c = 350;
+        System.out.println("This is int: "+c);
+        System.out.println("------------------");
+        long d = 12563L;
+        System.out.println("This is long: "+d);
+        System.out.println("------------------");
+
+        double e = 5.8;
+        System.out.println("This is double: "+e);
+        System.out.println("------------------");
+        float f = 5.9f;
+        System.out.println("This is float: "+f);
+        System.out.println("------------------");
+
+        char g = 'g';
+        System.out.println("This is Character: "+g);
+        System.out.println("------------------");
+        boolean h = false;
+        System.out.println("This is Boolean: "+h);
+        System.out.println("------------------");
+
+    }
+}
