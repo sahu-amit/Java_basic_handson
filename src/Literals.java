b23cc5d911f6182587fe1aa8621437619a1d6879
Amit Sahu <sahuamitlu@gmail.com>
2025-12-16 22:38:55 +0530

    Literals example class file

---
+++ src/Literals.java
@@ -0,0 +1,24 @@
+public class Literals {
+    public static void main() {
+        int x = 0b101;
+        double y = 10_00_000;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        //In this code we have Used below data types
+        // int & double
+        System.out.println("We have used data types : int & double");
+
+        //In this code we have Used below variables
+        //x & y
+        System.out.println("We have used variables : x & y");
+
+        //In this code we have Used below literals
+        //0b101 & 10_00_000
+        System.out.println("We have used Literals : 0b101 & 10_00_000");
+        System.out.println("Literals are constant value that can't be changed");
+
+        System.out.println("Variables store data values, while literals represent the constant values stored in variables.");
+    }
+}
