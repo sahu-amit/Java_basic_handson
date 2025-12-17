a2c6126775890b28700ac8b35cbfa686d6f65974
Amit Sahu <sahuamitlu@gmail.com>
2025-12-17 23:33:44 +0530

    Updated camments for understanding Operators type

--- src/Operators.java
+++ src/Operators.java
@@ -4,7 +4,7 @@ public class Operators {
             // write code here
             int a = 20;
             int b = 10;
-
+//Arithmatic operators
             int Add = a+b;
             System.out.println("Addition: "+Add);
             int subtract = a-b;
@@ -13,9 +13,11 @@ public class Operators {
             System.out.println("Multiplication: "+multiply);
             int divide  = a/b;
             System.out.println("Division: "+divide);
-
+//Relational Operators or Comparators
             boolean result = a>b;
             System.out.println("Is a greater than b? "+result);
+
+//Logical Operators
             result = a>b && b>0;
             System.out.println("Is a > b and b > 0? "+result);
 
