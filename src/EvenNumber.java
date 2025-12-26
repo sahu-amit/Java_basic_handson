56f9352f26ac3af9ae588d894a5dded60d868d88
Amit Sahu <sahuamitlu@gmail.com>
2025-12-27 03:21:29 +0530

    Practice

---
+++ src/EvenNumber.java
@@ -0,0 +1,15 @@
+public class EvenNumber {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i<=100; i++) {
+            if (i%2==0) {
+                System.out.print(i+ "\t ");
+// Printing even numbers row wise in groups of 10
+                count++;
+                if (count%10==0){
+                    System.out.println( );
+                }
+            }
+        }
+    }
+}
---
+++ src/NumbersUsingLoop.java
@@ -0,0 +1,10 @@
+public class NumbersUsingLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i<=10; i++){
+            for (int j = i; j<=100; j+= 10) {
+                System.out.print(j + "\t");
+            }
+        System.out.println( );
+        }
+    }
+}
